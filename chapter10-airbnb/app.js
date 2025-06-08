923dec3acb84a4d68a20ab761b87b24073a83c34
sauravKunwar-DataInsight <sauravkunwar63@gmail.com>
2025-06-08 11:34:58 +0545

    learnt about expressjs deepdive

--- chapter10-airbnb/app.js
+++ chapter10-airbnb/app.js
@@ -1,11 +1,25 @@
+//core module
+const path = require("path");
+
 //external module
 const express = require("express");
 
+//local module
+const userRouter = require("./routes/userRouter");
+
+const hostRouter = require("./routes/hostRouter");
+const rootDir = require("./utils/pathUtil")
+
 const app = express();
 
+app.use(express.urlencoded());
+
+app.use(userRouter);
+
+app.use("/host", hostRouter);
+
 app.use((req, res, next) => {
-  console.log(req.url, req.method);
-  res.send("<h1>Welcome to airbnb</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 const PORT = 3000;
---
+++ chapter10-airbnb/routes/userRouter.js
@@ -0,0 +1,16 @@
+//core modules
+const path = require("path");
+
+//external modules
+const express = require("express");
+const userRouter = express.Router();
+
+//local module
+
+const rootDir= require("../utils/pathUtil")
+
+userRouter.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "home.html"));
+});
+
+module.exports = userRouter;
